f0dcb65573c725f866f85e6f0132453227427749
Obie Hinojosa <obie@sarushinobie.dev>
2025-07-03 21:56:49 -0500

    fix variable name

--- lightshow/blink_rgb.py
+++ lightshow/blink_rgb.py
@@ -3,8 +3,8 @@ import machine, neopixel, time
 # defines `np`, pin the led is on = 16, number of leds = 1
 np = neopixel.NeoPixel(machine.Pin(16), 1)
 
-diddlydoobalobby = 1
-while diddlydoobalobby > 0:
+diddlydoobalooby = 1
+while diddlydoobalooby > 0:
     np[0] = (75, 0, 0)
     np.write()
     time.sleep(0.75)
